c567674dea05538ae6a8e955def41ce536a4376e
Bomberman93 <nikita.bondarenko.1993@gmail.com>
2022-02-15 15:30:47 +0200

    start

--- src/utils/imagesAxios.js
+++ src/utils/imagesAxios.js
@@ -6,16 +6,16 @@ const setParams = ({ q, page }) =>
   (axios.defaults.params = {
     q,
     page,
-    key: "25286831 - c1746f19640ae946dcd0d7543",
+    key: "25286831-c1746f19640ae946dcd0d7543",
     image_type: "photo",
     orientation: "horizontal",
     per_page: 12,
   });
 
-export const getImages = (q = 'cat', page = 1) => {
+export const getImages = (q = "cat", page = 1) => {
   setParams({ q, page });
   return axios
-    .get("/?")
+    .get("/")
     .then((res) => res.data.hits)
     .catch((err) => {
       throw err;
